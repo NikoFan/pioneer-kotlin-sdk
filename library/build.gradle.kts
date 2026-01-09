1d605aa3fbf885bb3c96c048a394589c5dc8c2cc
NikoFan <neoleg2005@gmail.com>
2026-01-09 16:12:50 +0300

    Изменения в файле сборки добавлены компоненты для подключения C++ и NDK добавлены компоненты для публикации в JitPack

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,17 +1,23 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("maven-publish") // Публикация в JitPack
 }
 
 android {
     namespace = "io.github.NikoFan.pioneer"
-    compileSdk = 36 // упрощённый синтаксис (без блока release)
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 24
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
+
+        // Подключение ndk для работы с дроном:
+         ndk {
+             abiFilters += listOf("arm64-v8a")
+         }
     }
 
     buildTypes {
@@ -32,8 +38,15 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-}
 
+     // Подключение Cmake
+     externalNativeBuild {
+         cmake {
+             path = file("src/main/cpp/CMakeLists.txt")
+             version = "3.22.1"
+         }
+     }
+}
 dependencies {
     // ✅ ОСТАВЛЯЕМ ТОЛЬКО ЭТО:
     // implementation("io.dronefleet.mavlink:mavlink:1.1.11")
@@ -45,4 +58,19 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+// === ПУБЛИКАЦИЯ ДЛЯ JITPACK ===
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                groupId = "com.github.NikoFan"       // ← GitHub username
+                artifactId = "pioneer-kotlin"       // ← имя репозитория
+                version = "1.0.0"                   // ← версия (лучше указывать в теге)
+
+                from(components["release"])
+            }
+        }
+    }
 }
